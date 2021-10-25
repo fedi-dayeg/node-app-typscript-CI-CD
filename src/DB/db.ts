1861b4336e82d99be3282e9fe60a075ff793fca5
fedi <fedi.dayeg@hotmail.com>
2021-10-25 20:18:18 +0100

    change the database url

--- src/DB/db.ts
+++ src/DB/db.ts
@@ -1,5 +1,5 @@
 import { Sequelize, Options, Op } from 'sequelize';
-const dbUrl: string = 'postgres://postgres:root@40.127.96.107/nodeApp' || '';
+const dbUrl: string = 'postgres://postgres:root@20.107.216.46/nodeApp' || '';
 const nodeEnv: string = process.env.NODE_ENV || '';
 
 if (!dbUrl) {
